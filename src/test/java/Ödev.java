e23c9c3d835676e8a34f8a6dece835b81eecbd7d
gulcanshnbs <gulcanshnbs36@gmail.com>
2022-12-21 22:00:09 +0100

    21.12.22

---
+++ src/test/java/Ödev.java
@@ -0,0 +1,14 @@
+public class Ödev {
+
+
+    //C01_TekrarTesti isimli bir class olusturun 2- https://www.google.com/ adresine gidin 3- cookies uyarisini kabul ederek kapatin
+    //4Sayfa basliginin “Google” ifadesi icerdigini test edin
+    //5Arama cubuguna “Nutella” yazip aratin
+    //6Bulunan sonuc sayisini yazdirin
+    //7sonuc sayisinin 10 milyon’dan fazla oldugunu test edin
+    //8Sayfayi kapatin
+
+
+
+
+}
